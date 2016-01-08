85c0cb5e9706fd498c846e4871975b5495fd5439
Kristina Clair <kclair@twilio.com>
2016-01-08 13:05:18 -0800

    move flickr functions into a class

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
 import sys
-import urllib2
-import json
-import random
 from flask import Flask, request, render_template
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 from flask_sqlalchemy import SQLAlchemy
+from app.flickr_search import FlickrSearch, LICENSE_CODES
 
 app = Flask(__name__)
 app.debug = True
@@ -16,18 +14,6 @@ db = SQLAlchemy(app)
 TW_SID = os.environ.get('TW_SID')
 TW_TOKEN = os.environ.get('TW_TOKEN')
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER') 
-
-LICENSE_CODES = {
-  '0': 'All Rights Reserved',
-  '1': 'Attribution-NonCommercial-ShareAlike License (http://creativecommons.org/licenses/by-nc-sa/2.0/)',
-  '2': 'Attribution-NonCommercial License (http://creativecommons.org/licenses/by-nc/2.0/)',
-  '3': 'Attribution-NonCommercial-NoDerivs License (http://creativecommons.org/licenses/by-nc-nd/2.0/)',
-  '4': 'Attribution License (http://creativecommons.org/licenses/by/2.0/)',
-  '5': 'Attribution-ShareAlike License (http://creativecommons.org/licenses/by-sa/2.0/)',
-  '6': 'Attribution-NoDerivs License (http://creativecommons.org/licenses/by-nd/2.0/)',
-  '7': 'No known copyright restrictions (http://flickr.com/commons/usage/)',
-  '8': 'United States Government Work (http://www.usa.gov/copyright.shtml)',
-}
 
 WELCOME_MESSAGE = '' \
     'Hello! Send any word or phrase (under 20 characters) ' \
@@ -44,58 +30,6 @@ def welcome_message():
 
 def twilio_auth():
     return TwilioRestClient(TW_SID, TW_TOKEN)
-
-def flickr_args(search_term):
-    return '&'.join([
-        'api_key=3ec65cdbfb9d1c0e566c056fd2ae4bc6',
-        'format=json',
-        'method=flickr.photos.search',
-        'content_type=1',
-        'extras=license,description',
-        'sort=relevance',
-        'text={}'.format(urllib2.quote(search_term)),
-        'nojsoncallback=1',
-        'safe_search=1'
-    ])
-
-def find_photo(data, search_term):
-    matching_photos = []
-    if 'photos' not in data:
-        return None
-    if 'photo' not in data['photos']:
-        return None
-    for photo in data['photos']['photo']:
-        match_strings = [
-            photo['title'],
-            photo['description']['_content']
-        ]
-        for s in match_strings:
-            if search_term.lower() in s.lower():
-                matching_photos.append(photo)
-                continue
-    try:
-        return random.choice(matching_photos)
-    except IndexError:
-        return None
-
-def construct_url_for_photo(photo):
-    if not photo:
-        return None
-    return 'https://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg'.format(
-        **photo)
-
-def search_flickr_for_term(search_term):
-    flickr_url = '?'.join([
-        'https://www.flickr.com/services/rest/',
-        flickr_args(search_term)
-    ]) 
-    response = urllib2.urlopen(flickr_url)
-    try:
-        data = json.load(response)
-    except ValueError:
-        return None
-    photo = find_photo(data=data,search_term=search_term) 
-    return (photo, construct_url_for_photo(photo))
 
 def lookup_number(number):
     phone_number = PhoneNumber.query.filter_by(phone_number=number).first()
@@ -138,7 +72,8 @@ def search_flickr():
     if not search_term:
        return return_error('search term not found. %s' % WELCOME_MESSAGE)
     search_term = search_term.replace('+', ' ') 
-    (photo, image_url) = search_flickr_for_term(search_term) 
+    f_search = FlickrSearch(search_term)
+    (photo, image_url) = f_search.search() 
     if not image_url:
        return return_error('no matching image found.')
     send_mms(to_number=from_number, image_url=image_url, photo=photo)
---
+++ app/flickr_search.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import urllib2
+import json
+import random
+
+LICENSE_CODES = {
+  '0': 'All Rights Reserved',
+  '1': 'Attribution-NonCommercial-ShareAlike License (http://creativecommons.org/licenses/by-nc-sa/2.0/)',
+  '2': 'Attribution-NonCommercial License (http://creativecommons.org/licenses/by-nc/2.0/)',
+  '3': 'Attribution-NonCommercial-NoDerivs License (http://creativecommons.org/licenses/by-nc-nd/2.0/)',
+  '4': 'Attribution License (http://creativecommons.org/licenses/by/2.0/)',
+  '5': 'Attribution-ShareAlike License (http://creativecommons.org/licenses/by-sa/2.0/)',
+  '6': 'Attribution-NoDerivs License (http://creativecommons.org/licenses/by-nd/2.0/)',
+  '7': 'No known copyright restrictions (http://flickr.com/commons/usage/)',
+  '8': 'United States Government Work (http://www.usa.gov/copyright.shtml)',
+}
+
+class FlickrSearch(object):
+    '''A class for handling flickr searches'''
+
+    def __init__(self, search_term):
+        self.search_term = search_term
+
+    @property
+    def request_args(self):
+        return '&'.join([
+            'api_key=3ec65cdbfb9d1c0e566c056fd2ae4bc6',
+            'format=json',
+            'method=flickr.photos.search',
+            'content_type=1',
+            'extras=license,description',
+            'sort=relevance',
+            'text={}'.format(urllib2.quote(self.search_term)),
+            'nojsoncallback=1',
+            'safe_search=1'
+        ])
+
+    def find_photo(self, data):
+        matching_photos = []
+        if 'photos' not in data:
+            return None
+        if 'photo' not in data['photos']:
+            return None
+        for photo in data['photos']['photo']:
+            match_strings = [
+                photo['title'],
+                photo['description']['_content']
+            ]
+            for s in match_strings:
+                if self.search_term.lower() in s.lower():
+                    matching_photos.append(photo)
+                    continue
+        try:
+            return random.choice(matching_photos)
+        except IndexError:
+            return None
+
+    def construct_url_for_photo(self, photo):
+        if not photo:
+            return None
+        return 'https://farm{farm}.staticflickr.com/{server}/{id}_{secret}.jpg'.format(
+            **photo)
+
+    def search(self):
+        flickr_url = '?'.join([
+            'https://www.flickr.com/services/rest/',
+            self.request_args(self.search_term)
+        ]) 
+        response = urllib2.urlopen(flickr_url)
+        try:
+            data = json.load(response)
+        except ValueError:
+            return None
+        photo = self.find_photo(data=data) 
+        return (photo, self.construct_url_for_photo(photo))

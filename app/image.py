26bc3ab671ad95a1cc6c66207a5094d814e35e65
Kristina Clair <kclair@twilio.com>
2016-01-19 11:37:44 -0800

    fix namespace of db variabele

--- app/image.py
+++ app/image.py
@@ -5,10 +5,10 @@ class Image(app_base.db.Model):
     flickr_id = app_base.db.Column(app_base.db.String(30))
     image_url = app_base.db.Column(app_base.db.String(255))
 
-    phone_number_id = db.Column(
-        db.Integer, db.ForeignKey('phone_number.phone_number'))
-    phone_number = db.relationship('PhoneNumber',
-        backref=db.backref('phone_numbers', lazy='dynamic')) 
+    phone_number_id = app_base.db.Column(
+        app_base.db.Integer, app_base.db.ForeignKey('phone_number.phone_number'))
+    phone_number = app_base.db.relationship('PhoneNumber',
+        backref=app_base.db.backref('phone_numbers', lazy='dynamic')) 
 
     def __init__(self, flickr_id, image_url, phone_number):
         self.flickr_id = flickr_id

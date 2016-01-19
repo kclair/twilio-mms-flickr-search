c7bbe880db1844abf7b61e762f3b6008309d8b1f
Kristina Clair <kclair@twilio.com>
2016-01-19 11:56:34 -0800

    add show option to manage_db

--- manage_db.py
+++ manage_db.py
@@ -2,9 +2,13 @@ import os
 import sys
 
 from app.app_base import db
+from app.phone_number import PhoneNumber
+from app.image import Image
 
 if sys.argv[1] == 'create':
     db.create_all()
 elif sys.argv[1] == 'drop':
     db.drop_all()
-
+elif sys.argv[1] == 'show':
+    print PhoneNumber.query.all()
+    print Image.query.all()

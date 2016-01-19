69b21742740935aa767dfabcd2c02949ce3268b5
Kristina Clair <kclair@twilio.com>
2016-01-19 11:45:51 -0800

    add id to phone_numbers table

--- app/image.py
+++ app/image.py
@@ -1,4 +1,5 @@
 import app_base
+import phone_number
 
 class Image(app_base.db.Model):
     id = app_base.db.Column(app_base.db.Integer, primary_key=True)
@@ -6,7 +7,7 @@ class Image(app_base.db.Model):
     image_url = app_base.db.Column(app_base.db.String(255))
 
     phone_number_id = app_base.db.Column(
-        app_base.db.Integer, app_base.db.ForeignKey('phone_number.phone_number'))
+        app_base.db.Integer, app_base.db.ForeignKey('phone_number.id'))
     phone_number = app_base.db.relationship('PhoneNumber',
         backref=app_base.db.backref('phone_numbers', lazy='dynamic')) 
 
@@ -15,7 +16,9 @@ class Image(app_base.db.Model):
         self.image_url = image_url
         self.phone_number = phone_number
 
-def store_image(photo, image_url, phone_number):
+def store_image(photo, image_url, from_number):
+    phone_number = PhoneNumber.query.filter_by(
+        phone_number=from_number).first() 
     img = Image(photo['id'], image_url, phone_number)
     app_base.db.session.add(img)
     app_base.db.session.commit()

eb3f9188a148280d1016620a69d662463f2a815d
Kristina Clair <kclair@twilio.com>
2016-10-24 12:51:54 -0400

    encourage alpaca searches

--- app/twilio_mms.py
+++ app/twilio_mms.py
@@ -50,7 +50,7 @@ class TwilioMms(object):
         except KeyError:
             license_text = 'Unknown.'
         if search_term.lower() == 'llama':
-            body = 'YAY LLAMAs!! Are you sure this is not an alpaca?'
+            body = 'YAY LLAMAS!! Maybe you should try an alpaca next.'
         else:
             body = 'Here are your search results. Next time you should consider searching for "llama".'
         self.client.messages.create(
